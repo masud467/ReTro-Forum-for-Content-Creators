01f8d81807b0a25ae9fd499a1c24206a99795ef5
masud4657 <masudpfrnt@gmail.com>
2024-03-04 23:15:17 +0600

    added the mobile responsive part.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const loadPost = async(categoryName=categoryName) => {
     const cards = data.posts
     console.log(cards)
     displayPost(cards)
+  
+    
 }
 
 
@@ -17,7 +19,7 @@ const displayPost = (cards) => {
         const postCard = document.createElement('div')
         postCard.classList = 'card card-side bg-base-100 shadow-xl p-7'
         postCard.innerHTML = `
-        <div  class="indicator">
+        <div id="indicator"  class="indicator">
         <figure class='w-16 h-16 rounded-2xl'><img src="${card.image}" alt="Movie"/></figure>
         <span id="indicator-color" class="badge badge-xs badge-primary indicator-item"></span>
         </div>
@@ -48,19 +50,31 @@ const displayPost = (cards) => {
                    </div>
                </div>
                 <div class="">
-                <img onclick="" src="icon/Vector.svg" alt="">
+                <img onclick="detailShow()" src="icon/Vector.svg" alt="">
                 </div>
                </div>
             </div>
         </div>`
 
         postContainer.appendChild(postCard)
+
+        
+    
     
     });   
 
+   
+    
+
     toggledLoadingSpinner(false)
 
+
+   
 }
+
+
+
+
 
 
 const handleSearch = () => {
@@ -82,6 +96,27 @@ const toggledLoadingSpinner = (isLoading) => {
     else{
         loadingSpinner.classList.add('hidden')
     }
+}
+
+const detailShow = () => {
+    const cardShowContainer = document.getElementById('card-show')
+    const cardShow = document.createElement('div')
+    
+        cardShow.innerHTML= `
+        <div class="flex gap-x-24">
+        
+    <div class="bg-white mt-5 p-7 flex gap-5">
+        <h1>10 Kids Unaware of Their Halloween Costume </h1>
+        <img class="w-5 h-5" src="icon/eye.png" alt="">
+        <p>1568</p>
+
+    </div>
+    
+    
+    `
+
+    
+
 }
 
 
@@ -107,7 +142,7 @@ const displayLatestPost = (posts) => {
   <div class="card-body">
    <div class="flex gap-3">
    <img src="icon/calender.png" alt="">
-   <h2 class="card-title">${post.author.posted_date}</h2>
+  <h2 class="card-title">${post.author?.posted_date? post.author.posted_date : 'No publish date'}</h2>
    </div>
     <p class="font-extrabold">${post.title}</p>
     <p>${post.description}</p>
@@ -115,7 +150,7 @@ const displayLatestPost = (posts) => {
     <img class='w-11 h-11 rounded-full mt-2'  src="${post.profile_image}" alt="Shoes" class="rounded-xl" />
     <div class="space-y-1">
     <p class="font-extrabold">${post.author.name}</p>
-    <p>${post.author.designation}</p>
+    <p>${post.author?.designation? post.author.designation : 'Unknown'}</p>
     </div>
     </div>
     
